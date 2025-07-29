5e527aba3c95294a9e2192ce310a6babf71520ad
Anurag-Wagh <waghanuragarjun.231cs166@nitk.edu.in>
2025-07-29 15:00:27 +0000

    fix: add missing AdminRoute import in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { AuthProvider } from './context/AuthContext';
 import { DarkModeProvider } from './context/DarkModeContext';
 import Layout from './components/Layout';
+import AdminRoute from './components/AdminRoute';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';

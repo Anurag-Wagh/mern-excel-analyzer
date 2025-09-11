c9776c62f6bf53f5a36a759af9741887224ae968
Anurag Wagh <waghanuragarjun.231cs166@nitk.edu.in>
2025-09-11 16:41:05 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -41,7 +41,7 @@ function Login() {
             onChange={handleChange}
             required
           />
-           <style>backgroundImage: url('client/public/bg.jpg')</style> 
+           <style>backgroundImage: url('/bg(1).jpg')</style> 
           <div className="relative mb-4">
             <input
               className="w-full p-3 border-2 border-blue-100 rounded-lg focus:outline-none focus:border-indigo-400 bg-blue-50 placeholder-gray-400 pr-10"
